464ac2e889fcd1b6243de414fea8c528241e1149
vip302 <42496022+vip302@users.noreply.github.com>
2019-07-22 14:50:22 +0800

    Add files via upload

---
+++ scripts/main.js
@@ -0,0 +1,6 @@
+var myHeading = document.querySelector('h2');
+myHeading.textContent = 'Hello world!';
+
+document.querySelector('html').onclick = function() {
+    alert('Ouch! Stop poking me!');
+}
